dab3bafe855cac367afb49e08b82332c97ed80a7
Jessy Hu <jesshuy12@gmail.com>
2019-02-06 21:01:29 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,7 +2,7 @@ def alphabetize(arr)
   esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"   #string values
   arr.sort_by do |word|   #generates an array ofthe original collection element and the mapped value
     word.split("").collect do |letter|   #splitting each world to the letter value
-      esperanto_alphabet.index(letter)
+      esperanto_alphabet.index(letter)   #returns index of the esp_alp for letter
     end
   end
 end
