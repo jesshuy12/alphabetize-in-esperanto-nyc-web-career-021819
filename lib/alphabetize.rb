84a063a8fc17e467801759394e04982ce16dcc0b
Jessy Hu <jesshuy12@gmail.com>
2019-02-06 20:59:05 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,8 @@
 def alphabetize(arr)
-  # code here
+  esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"   #string values
+  arr.sort_by do |word|                       
+    word.split("").collect do |letter|
+      esperanto_alphabet.index(letter)
+    end
+  end
 end
